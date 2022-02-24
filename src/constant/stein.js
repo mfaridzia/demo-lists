eaf18eb5a52ccd334be66ca8255c10139d6da102
Muhammad Farid Zia <muhfaridzia@gmail.com>
2022-02-24 08:39:29 +0700

    feat(stein): config stein resources

---
+++ src/constant/stein.js
@@ -0,0 +1,6 @@
+export default {
+  base_url: "https://stein.efishery.com/v1/storages/5e1edf521073e315924ceab4",
+  list: "list",
+  option_area: "option_area",
+  option_size: "option_size"
+};
---
+++ src/services/api.js
@@ -0,0 +1,29 @@
+import SteinStore from "stein-js-client";
+import SteinConfig from "../constant/stein";
+
+const store = new SteinStore(SteinConfig.base_url);
+
+export const option = {
+  area: () => store.read(SteinConfig.option_area),
+  size: () => store.read(SteinConfig.option_size)
+};
+
+export function getList () {
+  return store.read(SteinConfig.list);
+}
+
+export function getByID (uuid) {
+  return store.read(SteinConfig.list, { search: { uuid } });
+}
+
+export function createList (payload) { 
+ return store.append(SteinConfig.list, [payload]);
+}
+
+export function updateList (uuid, payload) {
+  return store.edit(SteinConfig.list, { search: { uuid }, set: payload });
+}
+
+export function deleteList (uuid) {
+  return store.delete(SteinConfig.list, { search: { uuid } });
+}
